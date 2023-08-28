911b25f6111f6fdd3c6b510f12abb49b4ceb280c
Anthony-D99 <anthonydisaf@yahoo.com>
2023-08-28 17:06:42 -0400

    Attempting to reformat LoginForm.js, SearchBook.js, and SignupForm.js to meet acceptance criteria by using newly created mutations.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,7 +8,10 @@ import {
   Row
 } from 'react-bootstrap';
 
-import Auth from '../utils/auth';
+import Auth from '../utils/auth'
+import { useMutation, useQuery } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
@@ -63,6 +66,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const [saveBook, { error, data }] = await useMutation(SAVE_BOOK);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -72,8 +76,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
+      const { data } = await saveBook({
+        variables: {...savedBookIds}
+      })
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
